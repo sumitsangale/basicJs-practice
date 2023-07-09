8455a215166b9fd19d99647ee26876ac96e95c2c
sumit sangale <sangalesumit12@gmail.com>
2023-07-09 15:33:53 +0530

    counter delima function added

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ console.log("commiting another changes in repo");
 function add(a, b) {
   return a + b;
 }
+
+//****conter delima
+function counterDelima() {
+  let count = 0;
+  return function countUpdate() {
+    count += 1;
+    console.log("count", count);
+  };
+}
+// const counter = counterDelima();
+// counter();
+// counter();
+// counter();
+// console.log(counter);
+//****counter deloma

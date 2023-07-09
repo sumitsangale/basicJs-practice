5f53e344eecbfc747b4781a69d9b4cddb9e89d50
sumit sangale <sangalesumit12@gmail.com>
2023-07-09 15:46:26 +0530

    sets in JS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("start running...");
-console.log("commiting another changes in repo");
+// console.log("commiting another changes in repo");
 
-function add(a, b) {
+function addition(a, b) {
   return a + b;
 }
 
@@ -19,3 +19,15 @@ function counterDelima() {
 // counter();
 // console.log(counter);
 //****counter deloma
+
+//**** set in JS */
+const letters = new Set(["a", "g", "o", "a", "o"]);
+// console.log(letters);
+let testArr = ["a", "g", "o", "a", "o"];
+
+let uniqueArr = new Set();
+testArr.forEach((value, i) => {
+  uniqueArr.add(value);
+});
+// console.log(uniqueArr);
+//****set in JS */
